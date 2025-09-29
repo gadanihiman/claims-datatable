fa8194115bd521d1b1eaf4d5e9973a6de2ac511c
gadanihiman <gadanihiman@gmail.com>
2025-09-30 04:56:39 +0700

    Removes unnecessary gap from main element
    Removes the `gap` property from the main element.
    
    This simplifies the layout and potentially improves rendering performance by removing an unnecessary style.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import DataTable from "@/components/Datatable";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20">
+      <main className="flex flex-col row-start-2 items-center sm:items-start">
 	      <h1 className="mb-4 text-2xl font-semibold">Claims</h1>
 	      <DataTable />
       </main>
